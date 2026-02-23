a268d81f4c84606ca21d6f193a075bb198173a21
eotjmo <tjmo333444@gmail.com>
2026-02-23 20:56:09 +0200

    Basic static Auth Form

---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+const signInInactive = document.querySelector('#sign-in-inactive-button');
+const signInDiv = document.querySelector('#sign-in');
+
+signInInactive.addEventListener('click', function() {
+
+});
