f8aa95a1ba399e0f49e10c8025ef911e49ad1798
eotjmo <tjmo333444@gmail.com>
2026-02-24 14:22:19 +0200

    Events for <inactive> buttons

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,38 @@
 const signInInactive = document.querySelector('#sign-in-inactive-button');
-const signInDiv = document.querySelector('#sign-in');
+const signDiv = document.querySelector('#sign-in');
 
-signInInactive.addEventListener('click', function() {
+const signUpInactiveBtn = document.querySelector('#sign-up-inactive-button');
+const signInInactiveBtn = document.querySelector('#sign-in-inactive-button');
+const signUpActiveBtn = document.querySelector('#sign-up-active-button');
+const signInActiveBtn = document.querySelector('#sign-in-active-button');
 
+const welcomeRegister = document.querySelector("#register-welcome");
+const welcomeLogin = document.querySelector("#login-welcome");
+
+const contentRegister = document.querySelector('#sign-up-content');
+const contentLogin = document.querySelector('#sign-in-content');
+
+signDiv.addEventListener('click', function(event) {
+    // signInDiv.classList.add('sign-in-active');
+    if (event.target === signUpInactiveBtn) {
+        // alert("TEst");
+        welcomeRegister.style.display = 'none';
+        welcomeLogin.style.display = 'inline';
+
+        contentRegister.style.display = 'inline';
+        contentLogin.style.display = 'none';
+
+    } else if (event.target === signInInactiveBtn) {
+        welcomeRegister.style.display = 'inline';
+        welcomeLogin.style.display = 'none';
+
+        contentRegister.style.display = 'none';
+        contentLogin.style.display = 'inline';
+        
+    } 
+
+    // else if (event.target === signInActiveBtn) {
+    //     contentRegister.style.display = 'inline';
+    //     contentLogin.style.display = 'none';
+    // }
 });
